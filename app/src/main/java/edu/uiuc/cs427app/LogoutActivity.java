18e6214ab0d957d9abf861e805c146aa38c2200f
Luxia Yin <luxiayin@Luxias-MBP.lan>
2023-10-26 20:20:48 -0700

    new updated version

--- app/src/main/java/edu/uiuc/cs427app/LogoutActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/LogoutActivity.java
@@ -5,7 +5,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,13 +14,7 @@ public class LogoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Button logoutButton = findViewById(R.id.btnLogout);
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                logoutMenu(LogoutActivity.this);
-            }
-        });
+        logoutMenu(this);
     }
 
     public void logout(View view) {
@@ -31,7 +24,6 @@ public class LogoutActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
-
 
     private void logoutMenu(LogoutActivity logoutActivity) {
         AlertDialog.Builder builder = new AlertDialog.Builder(logoutActivity);
--- app/src/main/java/edu/uiuc/cs427app/SignupActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/SignupActivity.java
@@ -2,19 +2,16 @@ package edu.uiuc.cs427app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 public class SignupActivity extends AppCompatActivity {
     EditText username, password, repassword;
     Button btnSignUp, btnSignIn;
     edu.uiuc.cs427app.DataHelper myDB;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,57 +25,49 @@ public class SignupActivity extends AppCompatActivity {
         btnSignIn = (Button) findViewById(R.id.btnSignIn);
         myDB = new edu.uiuc.cs427app.DataHelper(this);
 
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String user = username.getText().toString();
-                String pass = password.getText().toString();
-                String repass = repassword.getText().toString();
+        btnSignUp.setOnClickListener(view -> {
+            String user = username.getText().toString();
+            String pass = password.getText().toString();
+            String repass = repassword.getText().toString();
 
-                if (user.equals("") || pass.equals("") || repass.equals("")) {
-                    Toast.makeText(SignupActivity.this, "All fields are required", Toast.LENGTH_SHORT).show();
-                }
-                else
+            if (user.equals("") || pass.equals("") || repass.equals("")) {
+                Toast.makeText(SignupActivity.this, "All fields are required", Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                if(pass.equals(repass))
                 {
-                    if(pass.equals(repass))
+                    Boolean usercheckResult = myDB.checkUsername(user);
+                    if(!usercheckResult)
                     {
-                        Boolean usercheckResult = myDB.checkUsername(user);
-                        if(usercheckResult == false)
-                        {
-                            Boolean regResult = myDB.insertData(user,pass);
-                            if (regResult == true){
-                                Toast.makeText(SignupActivity.this, "Registered Successfully!", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
-                                startActivity(intent);
+                        Boolean regResult = myDB.insertData(user,pass);
+                        if (regResult){
+                            Toast.makeText(SignupActivity.this, "Registered Successfully!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                            startActivity(intent);
 
-                            }
-                            else
-                            {
-                                Toast.makeText(SignupActivity.this, "Registered Failed!", Toast.LENGTH_SHORT).show();
-                            }
                         }
                         else
                         {
-                            Toast.makeText(SignupActivity.this, "User already Exists.\n Please Sign In", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(SignupActivity.this, "Registered Failed!", Toast.LENGTH_SHORT).show();
                         }
-
                     }
                     else
                     {
-                        Toast.makeText(SignupActivity.this,"Password not Matching.",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SignupActivity.this, "User already Exists.\n Please Sign In", Toast.LENGTH_SHORT).show();
                     }
+
+                }
+                else
+                {
+                    Toast.makeText(SignupActivity.this,"Password not Matching.",Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        btnSignIn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
-                startActivity(intent);
-
-            }
-
+        btnSignIn.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+            startActivity(intent);
         });
     }
 }
