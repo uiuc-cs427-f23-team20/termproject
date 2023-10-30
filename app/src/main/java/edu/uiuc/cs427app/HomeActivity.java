94671f6d35b3e7c4f989f76649e23d2fc91b15ab
SiCheng Meng <km883@outlook.com>
2023-10-29 23:45:01 -0400

    Added comments and refactored some typos

--- app/src/main/java/edu/uiuc/cs427app/HomeActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/HomeActivity.java
@@ -18,10 +18,17 @@ import java.util.Map;
 import edu.uiuc.cs427app.databinding.ActivityHomeBinding;
 
 
+/**
+ * HomeActivity displays the user's home screen after login. It manages the UI settings,
+ * displays the user's cities, and provides options for viewing details of each city and adding new cities.
+ */
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
     edu.uiuc.cs427app.DataHelper myDB;
     private AppBarConfiguration appBarConfiguration;
     private ActivityHomeBinding binding;
+
+    // Called when the activity is first created. Initializes the user interface
+    // retrieves user-specific data from the database, and sets up event listeners for buttons.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // on HomeActivity init, display correct ui setting for user
@@ -53,30 +60,36 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         }
 
 
-        // Task #2
+        // Task #2: Dynamically populating user cities and handling button actions
         LinearLayout outerLayout = (LinearLayout) findViewById(R.id.details);
+        // Height and width configurations for UI elements
         int linearLayoutHeight = (int) getResources().getDimension(R.dimen.LinearLayout_height);
         LinearLayout.LayoutParams textViewParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, linearLayoutHeight);
         int buttonWidth = (int) getResources().getDimension(R.dimen.button_width);
         LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(buttonWidth, LinearLayout.LayoutParams.WRAP_CONTENT);
         buttonParams.weight = 1;
 
+        // Retrieve user's cities from the database and dynamically create UI elements
         Map<String, String> userCities = myDB.getUserCitiesIdToCityName(intent.getExtras().getString("username"));
         for (String cityId : userCities.keySet()) {
+            // Create a layout for each city
             LinearLayout cityLayout = new LinearLayout(this);
             cityLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
             cityLayout.setOrientation(LinearLayout.HORIZONTAL);
 
+            // Create TextView to display city name
             TextView cityNameView = new TextView(this);
             cityNameView.setLayoutParams(textViewParams);
             cityNameView.setText(userCities.get(cityId));
 
+            // Create Button to view city details
             Button addCityButton = new Button(this);
             addCityButton.setLayoutParams(buttonParams);
             addCityButton.setText("View Detail");
             addCityButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+                    // Handle button click, navigate to DetailsActivity with city details
                     Intent intent;
                     intent = new Intent(view.getContext(), DetailsActivity.class);
                     intent.putExtra("city", userCities.get(cityId));
@@ -86,17 +99,20 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 }
             });
 
+            // Add TextView and Button to the city's layout; Add the city's layout to the outer layout
             cityLayout.addView(cityNameView);
             cityLayout.addView(addCityButton);
             outerLayout.addView(cityLayout);
         }
 
+        // Button to add a new city
         Button buttonAddCity = new Button(this);
         buttonAddCity.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
         buttonAddCity.setText("Add a location");
         buttonAddCity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Handle button click, navigate to AddCitiesActivity with username
                 Intent intent;
                 intent = new Intent(view.getContext(), AddCitiesActivity.class);
                 intent.putExtra("username",extras.getString("username"));
@@ -105,6 +121,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         });
         outerLayout.addView(buttonAddCity);
 
+        // Logout button setup and click listener
         Button buttonLogout = findViewById(R.id.btnLogout);
         buttonLogout.setOnClickListener(this);
     }
