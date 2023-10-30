56064ab6f3dd8bdf479e9af77c03f0b901683e53
luxiay <132629709+luxiay@users.noreply.github.com>
2023-10-29 21:53:08 -0700

    Update COMMETS LoginActivity.java

--- app/src/main/java/edu/uiuc/cs427app/LoginActivity.java
+++ app/src/main/java/edu/uiuc/cs427app/LoginActivity.java
@@ -25,24 +25,27 @@ public class LoginActivity extends AppCompatActivity {
         btnResignup = (Button)findViewById(R.id.btnResignup);
         myDB = new edu.uiuc.cs427app.DataHelper(this);
 
-        // Initializing the Login components
+        // Initializing the Login component, Set up a click listener for the login button.
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String user = username.getText().toString();
                 String pass = password.getText().toString();
-
+                
+                // Check if username and password are not empty
                 if (user.equals("") || pass.equals("")){
                     Toast.makeText(LoginActivity.this, "All fields are required", Toast.LENGTH_SHORT).show();
                 } else {
+                    // Check username and password against the database
                     Boolean result = myDB.checkUsernamePassword(user,pass);
                     if(result == true){
+                        // If login is successful, navigate to the HomeActivity
                         Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
                         // pass username variable to HomeActivity to query for uiconfig
                         intent.putExtra("username",user);
                         startActivity(intent);
-
                     } else {
+                        // Show an error message for an invalid password
                         Toast.makeText(LoginActivity.this, "Invalid password", Toast.LENGTH_SHORT).show();
                     }
                 }
